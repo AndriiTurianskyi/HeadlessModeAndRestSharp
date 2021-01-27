570e8001ba3d35051a427b81414090ccb0bbaa4b
AndriiTurianskyi <andriy.turyansky@gmail.com>
2021-01-28 01:33:00 +0200

    fixed tests

--- HeadlessModeAndRestSharp/RestSharp.cs
+++ HeadlessModeAndRestSharp/RestSharp.cs
@@ -3,6 +3,7 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,34 +13,45 @@ namespace HeadlessModeAndRestSharp
     [TestFixture]
     class RestSharp
     {
-        RestClient client = new RestClient("https://greencity-user.azurewebsites.net");
-
+        RestClient restClient = new RestClient("https://greencity.azurewebsites.net");
+       private readonly string commentId;
         public string getAccessToken()
         {
-            JsonObject json1 = new JsonObject();
-            json1.Add("email", "xdknxusqvjeovowpfk@awdrt.com");
-            json1.Add("password", "Temp#001");
+            RestClient ownSecurityClient = new RestClient("https://greencity-user.azurewebsites.net");
             var request = new RestRequest("/ownSecurity/signIn", Method.POST);
-            request.AddParameter("application/json", json1, ParameterType.RequestBody);
-            IRestResponse restResponse = client.Execute(request);
+            request.AddParameter("application/json", "{\"email\":\"pruvat@gmail.com\", \"password\":\"1Test@test\"}", ParameterType.RequestBody);
+            IRestResponse restResponse = ownSecurityClient.Execute(request);
             Regex regex = new Regex("\"accessToken\":\"(.*)\",\"refreshToken");
             Match match = regex.Match(restResponse.Content);
             return match.Groups[1].Value;
         }
 
         [Test]
-        public void createReply()
+        public void createComment()
         {
             string accessToken = getAccessToken();
-            Console.WriteLine(accessToken);
-            JsonObject json2 = new JsonObject();
-            json2.Add("parentCommentId", "4992");
-            json2.Add("text", "create reply with restSharp");
-            var request = new RestRequest("/econews/comments/8759", Method.POST);
+            JsonObject commentData = new JsonObject();
+            commentData.Add("parentCommentId", "0");
+            commentData.Add("text", "create reply by restSharp");
+            var request = new RestRequest("/econews/comments/9257", Method.POST);
             request.AddHeader("Authorization", "Bearer " + accessToken);
-            request.AddParameter("application/json", json2, ParameterType.RequestBody);
-            IRestResponse restResponse = client.Execute(request);
-            Console.WriteLine(restResponse.Content);
+            request.AddParameter("application/json", commentData, ParameterType.RequestBody);
+            IRestResponse restResponse = restClient.Execute(request);
+            //write code, i should get comment id
+            Assert.AreEqual(restResponse.StatusCode, HttpStatusCode.Created);
+        }
+
+        [Test]
+        public void deleteComment()
+        {
+            string accessToken = getAccessToken();
+            JsonObject commentData = new JsonObject();
+            commentData.Add("id", commentId);
+            var request = new RestRequest("/econews/comments", Method.DELETE);
+            request.AddHeader("Authorization", "Bearer " + accessToken);
+            request.AddParameter("application/json", commentData, ParameterType.RequestBody);
+            IRestResponse restResponse = restClient.Execute(request);
+            Assert.AreEqual(restResponse.StatusCode, HttpStatusCode.Created);
         }
     }
 }
